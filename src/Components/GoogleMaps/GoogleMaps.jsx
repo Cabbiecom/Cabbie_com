105b303946d61777de5d5e5c5ef9cf09d2f1c9e9
18-anth <anthonycordova330@gmail.com>
2024-02-28 13:13:26 -0500

    maps config home

--- src/Components/GoogleMaps/GoogleMaps.jsx
+++ src/Components/GoogleMaps/GoogleMaps.jsx
@@ -1,21 +1,55 @@
+/* global google */
 import React, { useState, useEffect } from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-import { getDatabase, ref, onValue } from "firebase/database";
-
+import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
+import { getDatabase, ref as dbRef, onValue, set, get } from "firebase/database";
+import { getAuth } from "firebase/auth";
+import { auth } from '../../Data/Database';
+import { useAuthState } from "react-firebase-hooks/auth";
 const containerStyle = {
   width: '100%',
   height: '550px',
 };
 
-// Reemplaza 'YOUR_GOOGLE_MAPS_API_KEY' con tu clave de API de Google Maps
 const googleMapsApiKey = 'AIzaSyCExz27MMGSdeZw-l1-qReRPSfEUgNV4po';
 
 const MapsAPI = () => {
-  const [currentLocation, setCurrentLocation] = useState({ lat: null, lng: null }); // Valores predeterminados
+  const [user] = useAuthState(auth);
+  const [currentLocation, setCurrentLocation] = useState({ lat: null, lng: null });
   const [taxiLocations, settaxiLocations] = useState([]);
+  const [photoURL, setPhotoURL] = useState('');
+  const [userAvatar, setUserAvatar] = useState('');
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: googleMapsApiKey,
+  });
+
+  const database = getDatabase();
 
   useEffect(() => {
-    // Función para obtener la ubicación actual del usuario
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+
+    if (user) {
+      setPhotoURL(user.photoURL || '');
+      const userDetailsRef = dbRef(database, `Users/UserDetails/${user.uid}`);
+      get(userDetailsRef).then((snapshot) => {
+        if (snapshot.exists()) {
+          const userData = snapshot.val();
+          setPhotoURL(userData.imageUrl);
+        } else {
+          console.log("No se encontraron datos del usuario en la base de datos.");
+        }
+      }).catch((error) => {
+        console.error("Error al obtener datos del usuario:", error);
+      });
+      onValue(userDetailsRef, (snapshot) => {
+        const userDetails = snapshot.val();
+        if (userDetails && userDetails.imageUrl) {
+          setUserAvatar(userDetails.imageUrl);
+        }
+      });
+    }
 
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -32,39 +66,75 @@ const MapsAPI = () => {
       );
     } else {
       console.error("Geolocalización no soportada por este navegador.");
-      // Puedes establecer una ubicación por defecto o manejar este caso como prefieras
     }
 
-    const database = getDatabase();
-    const taxistasRef = ref(database, 'Users/UsersClient');
+    const taxistasRef = dbRef(database, 'Users/UsersClient');
 
     onValue(taxistasRef, (snapshot) => {
+
       const users = snapshot.val();
-      const taxiLocations = [];
-      Object.keys(users).forEach((key) => {
-        const user = users[key];
-        if (user.role === "taxista" && user.lat && user.lng) {
-          taxiLocations.push({ lat: user.lat, lng: user.lng });
-        }
-      });
-      settaxiLocations(taxiLocations);
+
+      if (users) {
+
+        const taxiLocationsTemp = [];
+        Object.keys(users).forEach((key) => {
+          const user = users[key];
+
+          if (user.role === "taxista" && user.lat && user.lng && user.imageUrl) {
+            const avatarIcon = {
+              url: user.imageUrl,
+              scaledSize: new google.maps.Size(50, 50),
+            };
+            taxiLocationsTemp.push({
+              lat: user.lat,
+              lng: user.lng,
+              avatar: avatarIcon,
+            });
+          }
+        });
+        settaxiLocations(taxiLocationsTemp);
+      } else {
+        console.log('No se encontraron usuarios o la referencia de Firebase no es válida.');
+      }
     });
-  }, []);
+  }, [user, database, isLoaded]);
+
+  if (loadError) {
+    return <div>Error cargando el mapa</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Cargando mapa...</div>;
+  }
 
   return (
-    <LoadScript googleMapsApiKey={googleMapsApiKey}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={currentLocation}
-        zoom={15}
-      >
-        {/* Marcador para la ubicación actual */}
-        {taxiLocations.map((location, index) => (
-          <Marker key={index} position={location} />
-        ))}
-      </GoogleMap>
-    </LoadScript>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={currentLocation.lat && currentLocation.lng ? currentLocation : undefined}
+      zoom={15}
+    >
+      {currentLocation.lat && currentLocation.lng && (
+        <Marker
+          position={currentLocation}
+          icon={{
+            url: photoURL,
+            scaledSize: new google.maps.Size(50, 50),
+          }}
+
+        />
+      )}
+      {taxiLocations.map((location, index) => {
+        console.log(location.avatar.url);
+        return (
+          <Marker
+            key={index}
+            position={{ lat: location.lat, lng: location.lng }}
+            icon={location.avatar} />
+        );
+      })}
+    </GoogleMap>
   );
 };
 
 export default MapsAPI;
+
