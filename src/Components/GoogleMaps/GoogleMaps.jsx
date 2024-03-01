8ffbcca5139791472a99ebb318b23fc1d8b149fd
18-anth <anthonycordova330@gmail.com>
2024-03-01 13:30:11 -0500

    Modification Maps

--- src/Components/GoogleMaps/GoogleMaps.jsx
+++ src/Components/GoogleMaps/GoogleMaps.jsx
@@ -4,11 +4,16 @@ import { auth } from "../../Data/Database";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { getDatabase, ref as dbRef, set, get } from "firebase/database";
 
-
 const MapsAPI = () => {
-  const [currentLocation, setCurrentLocation] = useState({ lat: null, lng: null });
+  const savedLocation = JSON.parse(localStorage.getItem("location"));
+  const savedPhotoURL = localStorage.getItem("photoURL");
+  const [currentLocation, setCurrentLocation] = useState(savedLocation || {
+    lat: null,
+    lng: null,
+  });
   const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
-  const [photoURL, setPhotoURL] = useState("");
+  const [photoURL, setPhotoURL] = useState(savedPhotoURL || "");
+
   const [taxiUsers, setTaxiUsers] = useState([]);
   const [user, loading, error] = useAuthState(auth);
   const mapContainerStyle = { height: "500px", width: "100%" };
@@ -21,14 +26,17 @@ const MapsAPI = () => {
 
   useEffect(() => {
     if (user) {
-      setPhotoURL(user.photoURL || "");
+      const newUserPhotoURL = user.photoURL || "";
+      setPhotoURL(newUserPhotoURL);
 
       const userRef = dbRef(database, `Users/UsersClient/${user.uid}`);
       get(userRef)
         .then((snapshot) => {
           if (snapshot.exists()) {
             const userData = snapshot.val();
-            setPhotoURL(userData.imageUrl);
+            const imageUrl = userData.imageUrl || newUserPhotoURL;
+            setPhotoURL(imageUrl);
+            localStorage.setItem("photoURL", imageUrl);
           } else {
             console.log(
               "No se encontraron datos del usuario en la base de datos."
@@ -46,43 +54,56 @@ const MapsAPI = () => {
       // Referencia a todos los usuarios clientes
       const usersRef = dbRef(database, "Users/UsersClient");
 
-      get(usersRef).then((snapshot) => {
-        if (snapshot.exists()) {
-          const usersData = snapshot.val();
-          // Filtrar por usuarios con el rol de "taxista"
-          const taxiUsersFiltered = Object.entries(usersData)
-            .filter(([key, value]) => value.role === "taxista")
-            .map(([key, value]) => ({ uid: key, ...value }));
-          setTaxiUsers(taxiUsersFiltered);
-        } else {
-          console.log("No se encontraron datos de usuarios en la base de datos.");
-        }
-      }).catch((error) => {
-        console.error("Error al obtener datos de los usuarios:", error);
-      });
+      get(usersRef)
+        .then((snapshot) => {
+          if (snapshot.exists()) {
+            const usersData = snapshot.val();
+            // Filtrar por usuarios con el rol de "taxista"
+            const taxiUsersFiltered = Object.entries(usersData)
+              .filter(([key, value]) => value.role === "taxista")
+              .map(([key, value]) => ({ uid: key, ...value }));
+            setTaxiUsers(taxiUsersFiltered);
+          } else {
+            console.log(
+              "No se encontraron datos de usuarios en la base de datos."
+            );
+          }
+        })
+        .catch((error) => {
+          console.error("Error al obtener datos de los usuarios:", error);
+        });
     }
   }, [user, database]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        setCurrentLocation({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        });
-      },
-      (error) => {
-        console.error("Error obteniendo la ubicación: ", error);
-        alert("Error al obtener la ubicación");
-      },
-      { enableHighAccuracy: true }
-    );
-  }, []);
 
-
+    if (!savedLocation && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const location = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          setCurrentLocation(location);
+          localStorage.setItem("location", JSON.stringify(location));
+        },
+        (error) => {
+          console.error("Error obteniendo la ubicación: ", error);
+          alert("Error al obtener la ubicación");
+        },
+        { enableHighAccuracy: true }
+      );
+    }
+  }, [savedLocation]);
 
   useEffect(() => {
-    if (isLoaded && currentLocation.lat && currentLocation.lng && mapRef.current) {
+    let marker;
+    if (
+      isLoaded &&
+      currentLocation.lat &&
+      currentLocation.lng &&
+      mapRef.current
+    ) {
       const marker = new window.google.maps.Marker({
         position: currentLocation,
         map: mapRef.current,
@@ -97,18 +118,23 @@ const MapsAPI = () => {
         marker.setMap(null); // Limpieza al desmontar el componente
       };
     }
-  }, [currentLocation, isLoaded]);
+
+  }, [currentLocation, isLoaded, photoURL]);
 
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={mapContainerStyle}
-      center={currentLocation.lat && currentLocation.lng ? currentLocation : undefined}
+      center={
+        currentLocation.lat && currentLocation.lng ? currentLocation : undefined
+      }
       zoom={15}
-      onLoad={map => mapRef.current = map}
+      onLoad={(map) => (mapRef.current = map)}
     >
       {/* Los marcadores se manejan a través de efectos secundarios */}
     </GoogleMap>
-  ) : <div>Loading...</div>;
+  ) : (
+    <div>Loading...</div>
+  );
 };
 
 export default MapsAPI;
